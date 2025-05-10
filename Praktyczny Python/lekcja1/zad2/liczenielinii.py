b3af535158d7db21761583ae2cf50de010d14f06
Gwiazdka09 <jakubgwiazdowski12@gmail.com>
2025-05-10 19:14:20 +0200

    abc

--- Praktyczny Python/lekcja1/zad2/liczenielinii.py
+++ Praktyczny Python/lekcja1/zad2/liczenielinii.py
@@ -6,8 +6,35 @@ if len(sys.argv) != 2:
 plik = sys.argv[1]
 
 try:
-    with open(plik, "r", encoding="utf-8") as f:
+    with open(plik, 'r', encoding='utf-8') as f:
         linie = f.readlines()
-        print(f"Liczba wszystkich lini: {len(linie)}")
+        print(f"Liczba wszystkich linii: {len(linie)}")
 except FileNotFoundError:
-    print(f"Plik: '{plik}' nie istnieje")
+    print(f"Plik '{plik}' nie istnieje.")
+
+try:
+    with open(plik, 'r', encoding='utf-8') as f:
+        linie = f.readlines()
+
+        # Ignorowanie pustych linii
+        non_empty_lines = [line for line in linie if line.strip() != '']
+        print(f"Liczba wszystkich linii: {len(non_empty_lines)}")
+
+except FileNotFoundError:
+    print(f"Plik '{plik}' nie nie istnieje.")
+
+
+try:
+    with open(plik, 'r', encoding='utf-8') as f:
+        linie = f.readlines()
+
+        # Ignorowanie pustych linii
+        non_empty_lines = [line for line in linie if line.strip() != '']
+
+        # Ignorowanie lini komentarzy
+        non_comment_lines = [line for line in non_empty_lines if not line.strip().startswith('#')]
+
+        print(f"Liczba wszystkich linii bez # i pustych: {len(non_comment_lines)}")
+
+except FileNotFoundError:
+    print(f"Plik '{plik}' nie nie istnieje.")
